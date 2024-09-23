ba6d962379c4c746d239bfb91e4403ca6dd35cec
AndresElacion <andreselacioniii@gmail.com>
2024-09-23 19:48:35 +0800

    crud done for client

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Client;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreClientRequest;
+use App\Http\Requests\UpdateClientRequest;
 use App\Http\Resources\ClientResource;
 
 class ClientController extends Controller
@@ -28,5 +29,27 @@ class ClientController extends Controller
         Client::create($client);
 
         return to_route('dashboard')->with('success', 'New client was created.');
+    }
+
+    public function edit(Client $client) {
+        return inertia('Client/Edit', [
+            'client' => new ClientResource($client)
+        ]);
+    }
+
+    public function update(UpdateClientRequest $request, Client $client) {
+        $data = $request->validated();
+
+        $client->update($data);
+
+        return to_route('dashboard')->with('success', "Client \"$client->name\" was updated.");
+    }
+
+    public function destroy(Client $client) {
+        $name = $client->name;
+
+        $client->delete();
+
+        return to_route('dashboard')->with('success', "Client \"$name\" was deleted.");
     }
 }
